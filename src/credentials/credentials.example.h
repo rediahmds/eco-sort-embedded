c790ff2eae2488ddf45276ba356e8f0d9a0e44b8
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-05-01 23:04:23 +0700

    feat: :sparkles: example credentials

---
+++ src/credentials/credentials.example.h
@@ -0,0 +1,13 @@
+
+/*
+    Credentials for the EcoSort project
+    This file contains the credentials for the WiFi access point and the WiFi network.
+    The credentials are used to connect to the WiFi network and to create the access point.
+    The credentials are stored in a separate file to keep them private and secure.
+    The file is not included in the repository to prevent accidental exposure of the credentials.
+
+    Rename credentials.example.h to credentials.h and fill in the credentials for your WiFi network.
+    The credentials are used in the main.cpp file to connect to the WiFi network and to create the access point.
+ */
+#define AP_NAME "Your AP Name"
+#define AP_PASSWORD "Your AP Password" // Leave empty for open AP
