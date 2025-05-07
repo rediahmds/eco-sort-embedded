51b4d7426337905d5a242321f7a194b5ac6d3d2b
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-05-07 22:03:19 +0700

    feat: :sparkles: configure wifi manager and ota

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 #include <NetWizard.h>
+#include <ElegantOTA.h>
 
 // rename credentials.example.h to credentials.h and fill in the credentials
 #include <credentials/credentials.h>
@@ -10,13 +11,23 @@ static NetWizard netWizard(&server);
 
 void setup()
 {
-  netWizard.setStrategy(NetWizardStrategy::NON_BLOCKING);
-  netWizard.autoConnect(AP_NAME, AP_PASSWORD);
-
   Serial.begin(9600);
+
+  netWizard.autoConnect(AP_NAME, AP_PASSWORD);
+  netWizard.setStrategy(NetWizardStrategy::NON_BLOCKING);
+
+  ElegantOTA.setAuth(OTA_USERNAME, OTA_PASSWORD);
+  ElegantOTA.begin(&server);
+
+  server.begin();
 }
 
 void loop()
 {
   netWizard.loop();
+  ElegantOTA.loop();
+
+  Serial.print("IP Address: ");
+  Serial.println(netWizard.localIP());
+  delay(1000);
 }
