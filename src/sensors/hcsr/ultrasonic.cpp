fdf661b5508b50ecb3e0ccd8b156b32c845e8806
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-05-26 12:02:03 +0700

    feat: :sparkles: second ultrasonic utility

--- src/sensors/hcsr/ultrasonic.cpp
+++ src/sensors/hcsr/ultrasonic.cpp
@@ -6,11 +6,20 @@ NewPing sonarNonOrganic(
     SENSOR_ECHO_NONORGANIC_PIN,
     MAX_DISTANCE);
 
-NewPing sonarOrganic();
+NewPing sonarOrganic(
+    SENSOR_TRIG_ORGANIC_PIN,
+    SENSOR_ECHO_ORGANIC_PIN,
+    MAX_DISTANCE);
 
 int readLevelBinNonOrganic()
 {
     int distance = sonarNonOrganic.ping_cm();
+    return distance;
+}
+
+int readLevelBinOrganic()
+{
+    int distance = sonarOrganic.ping_cm();
     return distance;
 }
 
@@ -19,6 +28,6 @@ void printLevels(const BinsLevel &params)
     lcdPrint({
         .row = 2,
         .column = 0,
-        .message = "Org: " + String(params.binsNonOrganic) + " cm",
+        .message = "O: " + String(params.binsNonOrganic) + " cm | N: " + String(params.binsOrganic) + " cm",
     });
 }
--- src/sensors/hcsr/ultrasonic.h
+++ src/sensors/hcsr/ultrasonic.h
@@ -11,11 +11,17 @@ struct BinsLevel
 
 const int SENSOR_TRIG_NONORGANIC_PIN = 32;
 const int SENSOR_ECHO_NONORGANIC_PIN = 33;
+
+const int SENSOR_TRIG_ORGANIC_PIN = 25;
+const int SENSOR_ECHO_ORGANIC_PIN = 26;
+
 const int MAX_DISTANCE = 100; // centimeters
 
 extern NewPing sonarNonOrganic;
+extern NewPing sonarOrganic;
 
 int readLevelBinNonOrganic();
+int readLevelBinOrganic();
 void printLevels(const BinsLevel &params);
 
 #endif
