3666ebaf13f7f9ef77ab82d5448dbb83375f8574
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-06-11 20:54:45 +0700

    feat: :sparkles: implement custom lcd class and configure iot mode

--- src/main.cpp
+++ src/main.cpp
@@ -8,83 +8,106 @@
 #include <NetWizard.h>
 
 #include <sensors/mq2/methane.h>
-#include <sensors/hcsr/ultrasonic.h>
-#include <outputs/lcd/lcd.h>
 #include <outputs/servo/servo.h>
+
+#include <Bins.h> // ultrasonic
+#include <Lcd.h>
 
 static WebServer server(80);
 static BlynkTimer timer;
 static NetWizard netMan(&server);
 
+static Bins organicWasteBin(
+    SENSOR_TRIG_ORGANIC_PIN,
+    SENSOR_ECHO_ORGANIC_PIN);
+static Bins anorganicWasteBin(
+    SENSOR_TRIG_ANORGANIC_PIN,
+    SENSOR_ECHO_ANORGANIC_PIN);
+
+static LCD lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
+
 const unsigned long blynkInterval = 5000L;
 
 int deg = 0;
 int methaneADC = 0;
-int binLevelNonOrganic = 0;
-int binLevelOrganic = 0;
+float binLevelNonOrganic = 0;
+float binLevelOrganic = 0;
 
 void sendSensorData();
+
+const bool IoTMode = false;
 
 void setup()
 {
   Serial.begin(9600);
-  lcdInit();
+  lcd.initialize();
 
-  lcdPrint({.message = " Setting up WiFi... "});
-  netMan.autoConnect(AP_NAME, AP_PASSWORD);
-  const IPAddress ip = netMan.localIP();
-  lcdPrint({
-      .message = "Connected!",
-      .delay = 2000,
-      .clear = true,
-  });
-  lcdPrint({.message = "IP : " + ip.toString()});
+  if (IoTMode)
+  {
+    lcd.printMessageAt(0, 0, " Setting up WiFi... ");
+    netMan.autoConnect(AP_NAME, AP_PASSWORD);
 
-  ElegantOTA.setAuth(OTA_USERNAME, OTA_PASSWORD);
-  ElegantOTA.begin(&server);
+    ElegantOTA.setAuth(OTA_USERNAME, OTA_PASSWORD);
+    ElegantOTA.begin(&server);
 
-  server.begin();
+    server.begin();
 
-  Blynk.config(BLYNK_AUTH_TOKEN);
-  timer.setInterval(blynkInterval, sendSensorData);
+    Blynk.config(BLYNK_AUTH_TOKEN);
+    timer.setInterval(blynkInterval, sendSensorData);
+  }
+
+  lcd.clear();
 
   servo.attach(SERVO_PIN);
-  servo.write(0);
+  servo.write(45);
 }
 
 void loop()
 {
-  ElegantOTA.loop();
-  netMan.loop();
+  if (IoTMode)
+  {
+    ElegantOTA.loop();
+    netMan.loop();
 
-  Blynk.run();
-  timer.run();
+    Blynk.run();
+    timer.run();
+  }
 
   methaneADC = readMethane();
   printMethane(methaneADC);
 
-  binLevelNonOrganic = readLevelBinNonOrganic();
-  binLevelOrganic = readLevelBinOrganic();
-  printLevels({
-      .binsNonOrganic = binLevelNonOrganic,
-      .binsOrganic = binLevelOrganic,
-  });
+  lcd.clearRow(1);
+  lcd.clearRow(2);
+  binLevelNonOrganic = anorganicWasteBin.readLevelPercentage();
+  binLevelOrganic = organicWasteBin.readLevelPercentage();
+  lcd.printMessageAt(0, 2, "O: " + String(binLevelOrganic) + "%");
+  lcd.printMessageAt(0, 3, "A: " + String(binLevelNonOrganic) + "%");
 
   delay(1000);
+}
+
+BLYNK_CONNECTED()
+{
+  const IPAddress ipv4 = netMan.localIP();
+  const char *ssid = netMan.getSSID();
+
+  Blynk.virtualWrite(V5, ipv4.toString());
+  Blynk.virtualWrite(V6, ssid);
 }
 
 BLYNK_WRITE(V0)
 {
   String inferenceResult = param.asString();
 
+  lcd.clearRow(3);
   if (inferenceResult == "organic")
   {
-    lcdPrint({.row = 3, .message = "Organic"});
+    lcd.printCentered(3, "Organic");
     servo.write(70);
   }
   else
   {
-    lcdPrint({.row = 3, .message = "Non Organic"});
+    lcd.printCentered(3, "Non-organic");
     servo.write(150);
   }
 }
