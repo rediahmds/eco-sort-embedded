e6a75a0323da47e6bd0f2006457a88ae2fe8d4f0
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-07-07 16:33:52 +0700

    feat: :sparkles: update messages shown in the lcd

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,11 @@ static BlynkTimer timer;
 static NetWizard netMan(&server);
 
 static Bins organicWasteBin(
-    SENSOR_TRIG_ORGANIC_PIN,
-    SENSOR_ECHO_ORGANIC_PIN);
+	SENSOR_TRIG_ORGANIC_PIN,
+	SENSOR_ECHO_ORGANIC_PIN);
 static Bins anorganicWasteBin(
-    SENSOR_TRIG_ANORGANIC_PIN,
-    SENSOR_ECHO_ANORGANIC_PIN);
+	SENSOR_TRIG_ANORGANIC_PIN,
+	SENSOR_ECHO_ANORGANIC_PIN);
 static MQ2 methaneSensor(SENSOR_MQ2_PIN);
 
 static LCD lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
@@ -40,10 +40,10 @@ void sendSensorData();
 
 void setup()
 {
-  Serial.begin(9600);
-  lcd.initialize();
+	Serial.begin(9600);
+	lcd.initialize();
 
-  netMan.autoConnect(AP_NAME, "");
+	netMan.autoConnect(AP_NAME, "");
 	
 	// Connect to specific AP without opening a portal, input WiFi auth manually
 	// netMan.connect("AP", "Pass");
@@ -53,105 +53,105 @@ void setup()
 	netMan.onPortalState([&](NetWizardPortalState state)
 						 { NetMan::handlePortalChanges(state, lcd); });
 
-  ElegantOTA.setAuth(OTA_USERNAME, OTA_PASSWORD);
-  ElegantOTA.begin(&server);
+	ElegantOTA.setAuth(OTA_USERNAME, OTA_PASSWORD);
+	ElegantOTA.begin(&server);
 
-  server.begin();
+	server.begin();
 
-  Blynk.config(BLYNK_AUTH_TOKEN);
-  timer.setInterval(blynkInterval, sendSensorData);
+	Blynk.config(BLYNK_AUTH_TOKEN);
+	timer.setInterval(blynkInterval, sendSensorData);
 
-  lcd.clear();
-  servo.attach(SERVO_PIN);
+	lcd.clear();
+	servo.attach(SERVO_PIN);
 }
 
 void loop()
 {
-  ElegantOTA.loop();
-  netMan.loop();
+	ElegantOTA.loop();
+	netMan.loop();
 
-  Blynk.run();
-  timer.run();
+	Blynk.run();
+	timer.run();
 
 	lcd.clearRow(0);
 	lcd.printMessageAt(0, 0, "Result: " + inferenceResult);
 
-  lcd.clearRow(1);
-  methaneADC = methaneSensor.readCH4();
-  lcd.printMessageAt(0, 1, "CH4: " + String(methaneADC));
+	lcd.clearRow(1);
+	methaneADC = methaneSensor.readCH4();
+	lcd.printMessageAt(0, 1, "CH4: " + String(methaneADC));
 
-  lcd.clearRow(2);
-  lcd.clearRow(3);
-  binLevelNonOrganic = anorganicWasteBin.readLevelPercentage();
-  binLevelOrganic = organicWasteBin.readLevelPercentage();
-  lcd.printMessageAt(0, 2, "O: " + String(binLevelOrganic) + "%");
-  lcd.printMessageAt(0, 3, "A: " + String(binLevelNonOrganic) + "%");
+	lcd.clearRow(2);
+	lcd.clearRow(3);
+	binLevelNonOrganic = anorganicWasteBin.readLevelPercentage();
+	binLevelOrganic = organicWasteBin.readLevelPercentage();
+	lcd.printMessageAt(0, 2, "O: " + String(binLevelOrganic) + "%");
+	lcd.printMessageAt(0, 3, "A: " + String(binLevelNonOrganic) + "%");
 
-  delay(1000);
+	delay(1000);
 }
 
 BLYNK_CONNECTED()
 {
-  const IPAddress ipv4 = netMan.localIP();
-  const char *ssid = netMan.getSSID();
+	const IPAddress ipv4 = netMan.localIP();
+	const char *ssid = netMan.getSSID();
 
-  Blynk.virtualWrite(V5, ipv4.toString());
-  Blynk.virtualWrite(V6, ssid);
+	Blynk.virtualWrite(V5, ipv4.toString());
+	Blynk.virtualWrite(V6, ssid);
 
-  const bool isServoAttached = servo.attached();
-  if (isServoAttached)
-  {
-    Blynk.virtualWrite(V7, "Attached");
-  }
-  else
-  {
-    Blynk.virtualWrite(V7, "Not Attached");
-  }
+	const bool isServoAttached = servo.attached();
+	if (isServoAttached)
+	{
+		Blynk.virtualWrite(V7, "Attached");
+	}
+	else
+	{
+		Blynk.virtualWrite(V7, "Not Attached");
+	}
 }
 
 BLYNK_WRITE(V0)
 {
 	inferenceResult = param.asString();
 
-  if (inferenceResult == "organic")
-  {
+	if (inferenceResult == "organic")
+	{
 		lcd.printMessageAt(0, 9, "Organic");
 		servo.tiltToOrganincBin();
 		delay(300);
-  }
-  else
-  {
+	}
+	else
+	{
 		lcd.printMessageAt(0, 9, "Non-organic");
 		servo.tiltToAnorganicBin();
 		delay(300);
-  }
+	}
 
 	servo.toInitialPosition();
 }
 
 BLYNK_WRITE(V1)
 {
-  deg = param.asInt();
-  servo.write(deg);
-  Serial.println("[BLYNK] V2 value changed");
+	deg = param.asInt();
+	servo.write(deg);
+	Serial.println("[BLYNK] V2 value changed");
 }
 
 BLYNK_WRITE(V8)
 {
-  const bool isResetPressed = param.asInt();
-  if (isResetPressed)
-  {
-    netMan.reset();
-    ESP.restart();
-  }
+	const bool isResetPressed = param.asInt();
+	if (isResetPressed)
+	{
+		netMan.reset();
+		ESP.restart();
+	}
 }
 
 void sendSensorData()
 {
-  Blynk.virtualWrite(V2, binLevelOrganic);
-  Blynk.virtualWrite(V3, binLevelNonOrganic);
+	Blynk.virtualWrite(V2, binLevelOrganic);
+	Blynk.virtualWrite(V3, binLevelNonOrganic);
 
-  Blynk.virtualWrite(V4, methaneADC);
+	Blynk.virtualWrite(V4, methaneADC);
 
-  Serial.println("Sensor data sent successfully!");
+	Serial.println("Sensor data sent successfully!");
 }
