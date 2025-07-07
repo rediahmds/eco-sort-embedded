67af7f16aa2273b53c55e44a886280fe721f5405
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-07-07 16:34:27 +0700

    feat: :sparkles: customized servo lib

--- lib/ServoPositioner/ServoPositioner.cpp
+++ lib/ServoPositioner/ServoPositioner.cpp
@@ -4,32 +4,32 @@ ServoPositioner::ServoPositioner(int pin) {}
 
 void ServoPositioner::toInitialPosition()
 {
-    // const bool isAttached = attached();
-    // if (!isAttached)
-    // {
-    //     attach(_pin);
-    // }
-    write(INITIAL_POSITION);
+	// const bool isAttached = attached();
+	// if (!isAttached)
+	// {
+	//     attach(_pin);
+	// }
+	write(INITIAL_POSITION);
 }
 
 void ServoPositioner::tiltToOrganincBin()
 {
-    // const bool isAttached = attached();
-    // if (!isAttached)
-    // {
-    //     attach(_pin);
-    // }
-    write(DEFAULT_DEGREE_ORGANIC);
+	// const bool isAttached = attached();
+	// if (!isAttached)
+	// {
+	//     attach(_pin);
+	// }
+	write(DEFAULT_DEGREE_ORGANIC);
 }
 
 void ServoPositioner::tiltToAnorganicBin()
 {
-    // const bool isAttached = attached();
-    // if (!isAttached)
-    // {
-    //     attach(_pin);
-    // }
-    write(DEFAULT_DEGREE_ANORGANIC);
+	// const bool isAttached = attached();
+	// if (!isAttached)
+	// {
+	//     attach(_pin);
+	// }
+	write(DEFAULT_DEGREE_ANORGANIC);
 }
 
 ServoPositioner::~ServoPositioner() {}
