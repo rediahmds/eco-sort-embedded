ab0e57ab26c3e7b5d377f9d30b125ca807d2325f
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-07-28 20:49:06 +0700

    chore: rewire ultrasonic sensor

--- lib/Bins/Bins.h
+++ lib/Bins/Bins.h
@@ -6,8 +6,8 @@
 const int SENSOR_TRIG_ANORGANIC_PIN = 32;
 const int SENSOR_ECHO_ANORGANIC_PIN = 33;
 
-const int SENSOR_TRIG_ORGANIC_PIN = 25;
-const int SENSOR_ECHO_ORGANIC_PIN = 26;
+const int SENSOR_TRIG_ORGANIC_PIN = 5;
+const int SENSOR_ECHO_ORGANIC_PIN = 18;
 
 const int MAX_DISTANCE = 100; // centimeters
 const int BIN_HEIGHT = 20;    // centimeters
